5cd59152c0761e92d0ab4d284a5e569f3c8a80d7
umralieva <111286266+umralieva@users.noreply.github.com>
2023-01-04 14:08:30 +0600

    Delete hw-2/my-app/src/hw-4/my-app directory

--- hw-2/my-app/src/hw-4/my-app/src/redux/reducers/index.js
+++
@@ -1,9 +0,0 @@
-import {combineReducers} from "redux";
-import userReducer from "./userReducer";
-
-
-
-export const rootReducer = combineReducers ({
-       userReducer
-
-})
--- hw-2/my-app/src/hw-4/my-app/src/redux/reducers/types.js
+++
@@ -1,5 +0,0 @@
-export const types = {
-    GET_USERS: "GET_USERS",
-    GET_ID:" GET_ID"
-
-}
